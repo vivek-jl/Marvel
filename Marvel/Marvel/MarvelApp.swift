bb4188438d7e355ceb2ae15f75c7eeb28fe81d7f
Vivek Jayakumar <vivek.l@go-jek.com>
2022-02-03 13:18:02 +0400

    Project Setup

---
+++ Marvel/Marvel/MarvelApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarvelApp.swift
+//  Marvel
+//
+//  Created by Vivek Jayakumar on 3/2/22.
+//
+
+import SwiftUI
+
+@main
+struct MarvelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
